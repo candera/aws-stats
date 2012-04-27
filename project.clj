99aa489d4e5544541338d8c9ee11a102244c51ae
Craig Andera <candera@wangdera.com>
2012-04-27 09:58:00 -0400

    Upgrade to Clojure 1.4 final.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject aws-stats "1.0.0-SNAPSHOT"
   :description "Analyzes AWS S3 logfiles"
-  :dependencies [[org.clojure/clojure "1.4.0-beta1"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
   :main aws-stats.core
   :aot [aws-stats.core])
