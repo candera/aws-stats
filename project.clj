20f06fc9c1864e188f8d08049be8c79e55f90c45
Craig Andera <candera@wangdera.com>
2013-07-27 08:17:59 -0400

    Update to latest version of dependencies.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject aws-stats "0.7.0"
   :description "Analyzes AWS S3 logfiles"
-  :dependencies [[clj-aws-s3 "0.3.3"]
-                 [com.datomic/datomic-free "0.8.3789"]
-                 [org.clojure/clojure "1.5.0-beta12"]
+  :dependencies [[clj-aws-s3 "0.3.6"]
+                 [com.datomic/datomic-free "0.8.4020.26"]
+                 [org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [org.clojure/tools.namespace "0.2.2"]
+                 [org.clojure/tools.namespace "0.2.4"]
                  [ring/ring-codec "1.0.0"]]
   :main aws-stats.main
   :profiles
